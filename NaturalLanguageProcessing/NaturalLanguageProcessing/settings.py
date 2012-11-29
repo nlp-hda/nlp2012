b3264dc1f98766699da1c4f25b8c5a0547133c83
David Claybourne <claybourne.david@gmail.com>
2012-11-29 21:47:55 +0100

    Created database and added classes for models

--- NaturalLanguageProcessing/NaturalLanguageProcessing/settings.py
+++ NaturalLanguageProcessing/NaturalLanguageProcessing/settings.py
@@ -126,6 +126,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'textdomain',
 )
 
 # A sample logging configuration. The only tangible logging
--- NaturalLanguageProcessing/textdomain/models.py
+++ NaturalLanguageProcessing/textdomain/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Term(models.Model):
+	name = models.CharField(max_length=50)
+
+class Word(models.Model):
+	name = models.CharField(max_length=100)
+
+class Text(models.Model):
+	name = models.CharField(max_length=50)
+	text = models.TextField()
+	words = models.ManyToManyField(Word, through='TextHasWords')
+
+class TextHasWords(models.Model):
+	text = models.ForeignKey(Text)
+	word = models.ForeignKey(Word)
+	count = models.IntegerField()	
+
+class Domain(models.Model):
+	name = models.CharField(max_length=50)
+	terms = models.ManyToManyField(Term)
+	texts = models.ManyToManyField(Text)
+	
