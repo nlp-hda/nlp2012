5ba880bbefb1e6f760e8a9059f19c13f72789600
David Claybourne <claybourne.david@gmail.com>
2012-12-11 16:54:51 +0100

    Added first part of frontend

--- NaturalLanguageProcessing/NaturalLanguageProcessing/urls.py
+++ NaturalLanguageProcessing/NaturalLanguageProcessing/urls.py
@@ -1,17 +1,18 @@
 from django.conf.urls import patterns, include, url
-
+from textdomain.views import home
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'NaturalLanguageProcessing.views.home', name='home'),
-    # url(r'^NaturalLanguageProcessing/', include('NaturalLanguageProcessing.foo.urls')),
+    # url(r'^/', 'textdomain.views.home', name='home'),
+    # url(r'^NaturalLanguageProcessing/', include('textdomain.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^home/', home),
 )
--- NaturalLanguageProcessing/textdomain/views.py
+++ NaturalLanguageProcessing/textdomain/views.py
@@ -1,1 +1,38 @@
-# Create your views here.
+from django.http import HttpResponse
+from textdomain.models import Domain, Text, Term, Word
+import datetime
+
+def home(request):
+	html = "<html><body>"
+	html += '<form action="/home/" method="GET">'
+	html += '<select name="id" onChange="this.form.submit()">'
+	texts = Text.objects.all()
+	html += "<option></option>"
+
+	for i in texts:
+		html += "<option value='%i'>%s</option>"%(int(i.id),i.name)
+
+	html += "</select>"
+	html += '</form>'
+	
+	id = 0
+	
+	if request.method == 'GET':
+		id = request.GET.get('id', '')
+		analyse = request.GET.get('analyse', '')
+		if id:
+			text = Text.objects.get(id=id)
+			html += "<h4>%s</h4>"%(text.name);
+			html += text.text
+			html += '<form action="/home/" method="GET">'
+			html += '<input type="hidden" name="analyse" value="1"/>'
+			html += '<input type="hidden" name="id" value="%i"/>'%(int(text.id))
+			html += '<input type="submit" value="Text analysieren ">'
+			html += '</form>'
+		if analyse:
+			html += "HAHA";
+	html += "</body></html>"
+	return HttpResponse(html)
+
+def test(request):
+    return HttpResponse("Test")
