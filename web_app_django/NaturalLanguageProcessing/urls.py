cbce0a4679812a64970207941888ec5d6f0df8c3
Muhammad <shahabhameed@gmail.com>
2013-01-09 16:03:50 +0100

    Start Server Bug Resolved - MSH

--- web_app_django/NaturalLanguageProcessing/urls.py
+++ web_app_django/NaturalLanguageProcessing/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-#from textdomain.views import home
+from textdomain import views
 
 from django.views.static import *
 from django.conf import settings
@@ -9,15 +9,19 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+
     # Examples:
-    # url(r'^/', 'textdomain.views.home', name='home'),
-    # url(r'^NaturalLanguageProcessing/', include('textdomain.urls')),
-    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-    # Uncomment the admin/doc line below to enable admin documentation:
+	url(r'^$', views.home, name='home'), #Fixed Bug during the start of the server (Shahab).
+	
+    url(r'^blacklist/$', views.blacklist, name='blacklist'),
+    url(r'^sentence/$', views.sentence, name='sentence'),
+	
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    
+	# Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^home/', include('textdomain.urls')),
-    #url(r'^home/', home),
 )
